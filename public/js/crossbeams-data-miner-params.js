5b1b05a17efffe89a636cccacaff0b6274881d69
James Silberbauer <jamessil@telkomsa.net>
2017-06-09 14:54:40 +0200

    Grid action columns. ESLint changes to javascript.

--- public/js/crossbeams-data-miner-params.js
+++ public/js/crossbeams-data-miner-params.js
@@ -1,77 +1,79 @@
+/* exported crossbeamsDataMinerParams */
 // Behaviour for parameter entry page of Crossbeams DataMiner reports.
 
-var crossbeamsDataMinerParams = {
+const crossbeamsDataMinerParams = {
 
   formId: '',
   reportNo: '',
-  query_params: [],
+  queryParams: {},
   current_values: [],
 
-  removeQueryParamItem: function(node) {
-      var index = crossbeamsUtils.getListIndex(node);
-      this.current_values.splice(index, 1);
-      node.parentNode.removeChild(node);
-    },
-
-  queryItemAsText: function(item) {
-      var val, val_to;
-      if(item.op === 'is_null' || item.op === 'not_null') {
-        val = '';
-        val_to = '';
-      }
-      else {
-        val = item.text;
-        if(item.op === 'between') {
-          val_to = ' AND ' + item.text_to;
-        }
-        else {
-          val_to = '';
-        }
-      }
-      //return '<li style="list-style-type:none;"><i class="fa fa-minus" style="cursor:pointer;color:red;" onclick="removeQueryParamItem(this.parentNode)"></i> ' + item.caption + ' ' + item.opText + ' ' + item.text;
-      return '<li style="list-style-type:none;"><i class="fa fa-minus" style="cursor:pointer;color:red;" onclick="crossbeamsDataMinerParams.removeQueryParamItem(this.parentNode)"></i> ' + item.caption + ' ' + item.opText + ' ' + val + val_to;
-    },
-
-  querySelectionAsText: function(param_values) {
-      if(param_values.length === 0) {
-        return '<li style="list-style-type:none;">None selected</li>';
-      }
-      else {
-        // var items = param_values.map(function(entry) {
-        //   return queryItemAsText(entry);
-        // });
-        var items = [];
-        for(var i=0;i<param_values.length;i++) {
-          items.push(crossbeamsDataMinerParams.queryItemAsText(param_values[i]));
-        }
-        return items.join('');
-      }
-    },
-
-  containsObject: function(obj, list) {
-    var i,hld;
-    for (i = 0; i < list.length; i++) {
-      hld = list[i];
-      //if (list[i] == obj) {
-      if (hld.col === obj.col && hld.op === obj.op && hld.val === obj.val && hld.val_to === obj.val_to) { // TODO: range of vals etc...
-        return true;
-      }
-    }
-
-    return false;
+  removeQueryParamItem: function removeQueryParamItem(node) {
+    const index = crossbeamsUtils.getListIndex(node);
+    this.current_values.splice(index, 1);
+    node.parentNode.removeChild(node);
   },
 
-  displayParamsAsText: function() {
-    var disp = '<ul>';
+  queryItemAsText: function queryItemAsText(item) {
+    let val = '';
+    let valTo = '';
+    if (item.op === 'is_null' || item.op === 'not_null') {
+      val = '';
+      valTo = '';
+    } else {
+      val = item.text;
+      if (item.op === 'between') {
+        valTo = ` AND ${item.textTo}`;
+      } else {
+        valTo = '';
+      }
+    }
+    // return '<li style="list-style-type:none;">
+    // <i class="fa fa-minus" style="cursor:pointer;color:red;"
+    // onclick="removeQueryParamItem(this.parentNode)">
+    // </i> ' + item.caption + ' ' + item.opText + ' ' + item.text;
+    return `<li style="list-style-type:none;"><i class="fa fa-minus" style="cursor:pointer;color:red;" onclick="crossbeamsDataMinerParams.removeQueryParamItem(this.parentNode)"></i> ${item.caption} ${item.opText} ${val}${valTo}`;
+  },
+
+  querySelectionAsText: function querySelectionAsText(paramValues) {
+    if (paramValues.length === 0) {
+      return '<li style="list-style-type:none;">None selected</li>';
+    }
+    // var items = paramValues.map(function (entry) {
+    //         return queryItemAsText(entry);
+    // });
+    const items = [];
+    paramValues.forEach((item) => {
+      items.push(crossbeamsDataMinerParams.queryItemAsText(item));
+    });
+    // for (i=0;i<paramValues.length;i++) {
+    //     items.push(crossbeamsDataMinerParams.queryItemAsText(paramValues[i]));
+    // }
+    return items.join('');
+  },
+
+  containsObject: function containsObject(obj, list) {
+    let foundMatch = false;
+    list.forEach((item) => {
+      if (item.col === obj.col && item.op === obj.op && item.val === obj.val &&
+        item.valTo === obj.valTo) { // TODO: range of vals etc...
+        foundMatch = true;
+      }
+    });
+    return foundMatch;
+  },
+
+  displayParamsAsText: function displayParamsAsText() {
+    let disp = '<ul>';
     disp += this.querySelectionAsText(this.current_values);
     disp += '</ul>';
     document.getElementById('param_display').innerHTML = disp;
   },
 
-  operatorParmChange: function(event) {
-    var val1 = document.getElementById('qp_value');
-    var val2 = document.getElementById('qp_value_to');
-    switch(event.target.value) {
+  operatorParmChange: function operatorParmChange(event) {
+    const val1 = document.getElementById('qp_value');
+    const val2 = document.getElementById('qp_value_to');
+    switch (event.target.value) {
       case 'between':
         val1.style.display = '';
         val2.style.display = '';
@@ -91,51 +93,59 @@ var crossbeamsDataMinerParams = {
   },
 
   // choice object: {
-  // col      : query column name
-  // op       : operator (=,>...)
-  // opText   : operater (is, greater than...)
-  // val      : selected/input value
-  // val_to   : second value for date ranges
-  // text     : selected value as text (e.g. for dropdown this could differ from the value)
-  // text_to  : second value for date ranges as text
+  // col                        : query column name
+  // op                         : operator (=,>...)
+  // opText         : operater (is, greater than...)
+  // val                        : selected/input value
+  // valTo         : second value for date ranges
+  // text          : selected value as text
+  // (e.g. for dropdown this could differ from the value)
+  // textTo        : second value for date ranges as text
   // }
-  addQpFormParam: function() {
-    var choice    = {};
-    var valElem   = document.getElementById('qp_value');
-    var valElem2  = document.getElementById('qp_value_to');
-    choice.col    = document.getElementById('qp_column').value;
-    choice.op     = document.getElementById('qp_operator').value;
+  addQpFormParam: function addQpFormParam() {
+    const choice = {};
+    const valElem = document.getElementById('qp_value');
+    const valElem2 = document.getElementById('qp_value_to');
+    choice.col = document.getElementById('qp_column').value;
+    choice.op = document.getElementById('qp_operator').value;
     choice.opText = document.getElementById('qp_operator').options[document.getElementById('qp_operator').selectedIndex].text;
-    choice.val    = valElem.value;
-    choice.val_to = valElem2.value;
-    var qp        = this.query_params[choice.col];
-    if(qp.control_type === 'list') {
-      choice.text    = valElem.options[valElem.selectedIndex].text;
-      choice.text_to = '';
+    choice.val = valElem.value;
+    choice.valTo = valElem2.value;
+    const qp = this.queryParams[choice.col];
+    if (qp.control_type === 'list') {
+      choice.text = valElem.options[valElem.selectedIndex].text;
+      choice.textTo = '';
+    } else {
+      choice.text = choice.val;
+      choice.textTo = choice.valTo;
     }
-    else {
-      choice.text    = choice.val;
-      choice.text_to = choice.val_to
+    choice.caption = qp.caption;
+    // if something other than is null or not null, 1st val must be present.
+    // If between, both must be present...
+    if (choice.op !== 'is_null' && choice.op !== 'not_null') {
+      if (choice.val === '') {
+        return false;
+      }
     }
-    choice.caption  = qp.caption;
-    // if something other than is null or not null, 1st val must be present. If between, both must be present...
-    if(choice.op !== 'is_null' && choice.op !== 'not_null') {
-      if(choice.val === '') { return false; }
+    if (choice.op === 'between') {
+      if (choice.valTo === '') {
+        return false;
+      }
     }
-    if(choice.op === 'between') {
-      if(choice.val_to === '') { return false; }
-    }
-    if(!crossbeamsDataMinerParams.containsObject(choice, this.current_values)) {
+    if (!crossbeamsDataMinerParams.containsObject(choice, this.current_values)) {
       this.current_values.push(choice);
       crossbeamsDataMinerParams.displayParamsAsText();
     }
+    return true;
   },
+
   // Listeners for change of parameters and of operators.
-  checkNode: function(addedNode) {
-    if (addedNode.nodeType === 1){
-      if (addedNode.matches('#qp_operator')){
+  checkNode: function checkNode(addedNode) {
+    let event = null;
+    if (addedNode.nodeType === 1) {
+      if (addedNode.matches('#qp_operator')) {
         addedNode.addEventListener('change', crossbeamsDataMinerParams.operatorParmChange);
-        var event = document.createEvent('HTMLEvents');
+        event = document.createEvent('HTMLEvents');
         event.initEvent('change', true, false);
         addedNode.dispatchEvent(event);
       }
@@ -147,101 +157,159 @@ var crossbeamsDataMinerParams = {
   // key:...url, including report
   // val: {limit: ; offset: ; current_values: }
   // .. on submit form, store
-  // .. on load, pass "back" to init to reload params.
+  // .. on load, pass 'back' to init to reload params.
   // Maybe store previous n dm params?
-  storeCurrentParams: function() {
-    var limit  = document.querySelector('#' + this.formId + " input[name=limit]").value;
-    var offset = document.querySelector('#' + this.formId + " input[name=offset]").value;
-    var stored = {limit: limit, offset: offset, paramValues: this.current_values};
+  storeCurrentParams: function storeCurrentParams() {
+    const limit = document.querySelector(`#${this.formId} input[name=limit]`).value;
+    const offset = document.querySelector(`#${this.formId} input[name=offset]`).value;
+    const stored = { limit, offset, paramValues: this.current_values };
 
-    var key = crossbeamsLocalStorage.genStandardKey(this.reportNo);
+    const key = crossbeamsLocalStorage.genStandardKey(this.reportNo);
     crossbeamsLocalStorage.setItem(key, stored);
   },
 
-  loadCurrentParams: function() {
-    var key = crossbeamsLocalStorage.genStandardKey(this.reportNo);
-    if(crossbeamsLocalStorage.hasItem(key)) {
-      //console.log('LOADING', this.reportNo, key);
-      var stored = crossbeamsLocalStorage.getItem(key);
-      document.querySelector('#' + this.formId + " input[name=limit]").value = stored.limit;
-      document.querySelector('#' + this.formId + " input[name=offset]").value = stored.offset;
+  loadCurrentParams: function loadCurrentParams() {
+    const key = crossbeamsLocalStorage.genStandardKey(this.reportNo);
+    let stored = null;
+    if (crossbeamsLocalStorage.hasItem(key)) {
+      // console.log('LOADING', this.reportNo, key);
+      stored = crossbeamsLocalStorage.getItem(key);
+      document.querySelector(`#${this.formId} input[name=limit]`).value = stored.limit;
+      document.querySelector(`#${this.formId} input[name=offset]`).value = stored.offset;
       this.current_values = stored.paramValues;
       crossbeamsDataMinerParams.displayParamsAsText();
     }
   },
 
-  buildReloadButton: function() {
-    var key = crossbeamsLocalStorage.genStandardKey(this.reportNo);
-    if(crossbeamsLocalStorage.hasItem(key)) {
+  buildReloadButton: function buildReloadButton() {
+    const key = crossbeamsLocalStorage.genStandardKey(this.reportNo);
+    if (crossbeamsLocalStorage.hasItem(key)) {
       document.querySelector('#reloadParams').style.display = '';
     }
   },
 
-  init: function(form_id, rpt_id, qprm) {
-    var observer;
-    this.formId       = form_id;
-    this.reportNo     = rpt_id;
-    this.query_params = qprm;
-
-    document.getElementById("select_param").addEventListener('change', function (event) {
-      var val = event.target.value, qp, val_input, input_type;
-      var qpForm = document.getElementById("qp_form");
-      if(val ==='') {
-        qpForm.innerHTML = '';
+  applyDefaultValues: function applyDefaultValues(queryParams) {
+    // for (qp in queryParams) { if (queryParams[qp].hasOwnProperty('default_value')) {
+    // console.log(queryParams[qp]['default_value']); } }
+    const choice = {};
+    let elem = null;
+    Object.keys(queryParams).forEach((qp) => {
+      if (queryParams[qp].default_value) {
+        // console.log(queryParams[qp]['default_value']);
+        elem = queryParams[qp];
+        choice.col = elem.column;
+        choice.op = '=';
+        choice.opText = 'is';
+        choice.val = elem.default_value;
+        choice.text = elem.default_value;
+        choice.valTo = '';
+        choice.textTo = '';
+        choice.caption = elem.caption;
+        this.current_values.push(choice);
       }
-      else {
-        qp = crossbeamsDataMinerParams.query_params[val];
-        input_type = '';
-        switch(qp.data_type) {
+    });
+    // for (qp in queryParams) {
+    //     if (queryParams[qp].default_value) {
+    //         // console.log(queryParams[qp]['default_value']);
+    //         elem = queryParams[qp];
+    //         choice.col = elem.column;
+    //         choice.op = "=";
+    //         choice.opText = "is";
+    //         choice.val = elem.default_value;
+    //         choice.text = elem.default_value;
+    //         choice.valTo = "";
+    //         choice.textTo = "";
+    //         choice.caption = elem.caption;
+    //         this.current_values.push(choice);
+    //     }
+    // }
+  },
+
+  init: function init(inFormId, inRptId, qprm) {
+    let observer = null;
+    this.formId = inFormId;
+    this.reportNo = inRptId;
+    this.queryParams = qprm;
+
+    document.getElementById('select_param').addEventListener('change', (event) => {
+      const val = event.target.value;
+      let qp = null;
+      let valInput = null;
+      let inputType = null;
+      const qpForm = document.getElementById('qp_form');
+      if (val === '') {
+        qpForm.innerHTML = '';
+      } else {
+        qp = crossbeamsDataMinerParams.queryParams[val];
+        inputType = '';
+        switch (qp.data_type) {
           case 'number':
-            input_type = 'type="number"';
+            inputType = 'type="number"';
             break;
           case 'integer':
-            input_type = 'type="number" pattern="\d*"'; // Allow for minus sign?...
+            inputType = 'type="number" pattern="\\d*"'; // Allow for minus sign?...
             break;
           case 'date':
-            input_type = 'type="date"';
+            inputType = 'type="date"';
             break;
           case 'datetime':
-            input_type = 'type="datetime-local"';
+            inputType = 'type="datetime-local"';
+            break;
+
+          // no default
         }
 
-        if(qp.control_type === 'list') {
-          val_input = crossbeamsUtils.makeSelect('qp_value', qp.list_values) +
-                        '<input name="qp_value_to" id="qp_value_to" style="display:none" />';
+        if (qp.control_type === 'list') {
+          valInput = `${crossbeamsUtils.makeSelect('qp_value', qp.list_values)}
+            <input name="qp_value_to" id="qp_value_to" style="display:none" />`;
+        } else if (qp.control_type === 'daterange') {
+          valInput = `<input name="qp_value" id="qp_value" value="${
+            (qp.default_value === null
+              ? ''
+                : qp.default_value)}" ${inputType} />
+            <input name="qp_value_to" id="qp_value_to" ${inputType} />`;
+        } else {
+          valInput = `<input name="qp_value" id="qp_value" value="${
+          (qp.default_value === null
+            ? ''
+              : qp.default_value)}" ${inputType} />
+            <input name="qp_value_to" id="qp_value_to" style="display:none"${inputType} />`;
         }
-        else {
-          if (qp.control_type === 'daterange') {
-            val_input = '<input name="qp_value" id="qp_value" value="'+ (qp.default_value === null ? '' : qp.default_value) +'" ' + input_type + ' />' +
-                        '<input name="qp_value_to" id="qp_value_to" ' + input_type + ' />';
-          }
-          else {
-            val_input = '<input name="qp_value" id="qp_value" value="'+ (qp.default_value === null ? '' : qp.default_value) +'"' + input_type + ' />' +
-                        '<input name="qp_value_to" id="qp_value_to" style="display:none"' + input_type + ' />';
-          }
-        }
-        qpForm.innerHTML = '<form id="dForm" action="#">' + '<input type="hidden" id="qp_column" value="' + qp.column + '" />' +
-          crossbeamsUtils.makeSelect('qp_operator', qp.operator) +
-          val_input +'<button type="button" class="pure-button pure-button" onclick="crossbeamsDataMinerParams.addQpFormParam()"><i class="fa fa-plus"></i></button></form>';
+        qpForm.innerHTML = `<form id="dForm" action="">
+          <input type="hidden" id="qp_column" value="${qp.column}" />
+          ${crossbeamsUtils.makeSelect('qp_operator', qp.operator)}
+          ${valInput}
+          <button type="button" class="pure-button pure-button"
+          onclick="crossbeamsDataMinerParams.addQpFormParam()">
+          <i class="fa fa-plus"></i></button></form>`;
       }
-            // event.stopPropagation();
-            // event.preventDefault();
+      // event.stopPropagation();
+      // event.preventDefault();
     });
+
+    // Apply default parameter values.
+    // console.log('qp', crossbeamsDataMinerParams.queryParams);
+
+    crossbeamsDataMinerParams.applyDefaultValues(crossbeamsDataMinerParams.queryParams);
 
     crossbeamsDataMinerParams.displayParamsAsText();
 
-    observer = new MutationObserver(function(mutations){
-      for (var i=0; i < mutations.length; i++){
-        for (var j=0; j < mutations[i].addedNodes.length; j++){
-          crossbeamsDataMinerParams.checkNode(mutations[i].addedNodes[j]);
-        }
-      }
+    observer = new MutationObserver((mutations) => {
+      mutations.forEach((node) => {
+        node.addedNodes.forEach((addedNode) => {
+          crossbeamsDataMinerParams.checkNode(addedNode);
+        });
+      });
+      // for (i=0; i < mutations.length; i++){
+      //     for (j=0; j < mutations[i].addedNodes.length; j++){
+      //         crossbeamsDataMinerParams.checkNode(mutations[i].addedNodes[j]);
+      //     }
+      // }
     });
 
     observer.observe(document.documentElement, {
       childList: true,
-      subtree: true
+      subtree: true,
     });
-  }
-
+  },
 };
--- public/js/crossbeams-utils.js
+++ public/js/crossbeams-utils.js
@@ -1,77 +1,98 @@
+/* exported crossbeamsUtils */
 // General utility functions for Crossbeams.
 
-var crossbeamsUtils = {
+const crossbeamsUtils = {
 
   // Toggle the visibility of en element in the DOM:
   // Optionally pass in a button to add the pure-button-active class (Pure.css)
-  toggle_visibility: function(id, button) {
-    var e = document.getElementById(id);
+  toggleVisibility: function toggleVisibility(id, button) {
+    const e = document.getElementById(id);
 
-    if ( e.style.display == 'block' ) {
+    if (e.style.display === 'block') {
       e.style.display = 'none';
-      if(button !== undefined) {
+      if (button !== undefined) {
         button.classList.remove('pure-button-active');
       }
-    }
-    else {
+    } else {
       e.style.display = 'block';
-      if(button !== undefined) {
+      if (button !== undefined) {
         button.classList.add('pure-button-active');
       }
     }
   },
 
-  getCharCodeFromEvent: function(event) {
-    event = event || window.event;
-    return (typeof event.which == "undefined") ? event.keyCode : event.which;
+  /**
+   * confirm() Shows a SweetAlert2 dialog asking the user to confirm or cancel.
+   * @param ...
+   */
+  confirm: function confirm({ prompt, title, okFunc, cancelFunc }) {
+    // console.log(title);
+    swal({
+      title: title === undefined ? '' : title,
+      text: prompt,
+      type: 'warning',
+      showCancelButton: true }).then(okFunc, cancelFunc).catch(swal.noop);
   },
 
-  isCharNumeric: function(charStr) {
-    return !!/\d/.test(charStr);
+  getCharCodeFromEvent: function getCharCodeFromEvent(evt) {
+    const event = evt || window.event;
+    return (event.which === 'undefined')
+      ? event.keyCode
+      : event.which;
   },
 
-  isKeyPressedNumeric: function(event) {
-    var charCode = this.getCharCodeFromEvent(event);
-    var charStr = String.fromCharCode(charCode);
+  isCharNumeric: function isCharNumeric(charStr) {
+    return !!(/\d/.test(charStr));
+  },
+
+  isKeyPressedNumeric: function isKeyPressedNumeric(event) {
+    const charCode = this.getCharCodeFromEvent(event);
+    const charStr = String.fromCharCode(charCode);
     return this.isCharNumeric(charStr);
-
   },
 
   // Make a select tag using an array for the options.
   // The Array can be an Array of Arrays too.
-  makeSelect: function(name, arr) {
-    var sel = '<select id="' + name + '" name="' + name + '">';
-    for(var i=0;i<arr.length;i++) {
-      if(arr[i].constructor === Array) {
-        sel += '<option value="' + (arr[i][1] || arr[i][0]) + '">' + arr[i][0] + '</option>';
+  makeSelect: function makeSelect(name, arr) {
+    // var sel = '<select id="' + name + '" name="' + name + '">';
+    let sel = `<select id="${name}" name="${name}">`;
+    arr.forEach((item) => {
+      if (item.constructor === Array) {
+        // sel += '<option value="' + (item[1] || item[0]) + '">' + item[0] + '</option>';
+        sel += `<option value="${(item[1] || item[0])}">${item[0]}</option>`;
+      } else {
+        // sel += '<option value="' + item + '">' + item + '</option>';
+        sel += `<option value="${item}">${item}</option>`;
       }
-      else {
-        sel += '<option value="' + arr[i] + '">' + arr[i] + '</option>';
-      }
-    }
+    });
     sel += '</select>';
     return sel;
   },
 
   // Adds a parameter named "json_var" to a form
   // containing a stringified version of the passed object.
-  addJSONVarToForm: function(formId, jsonVar) {
-    var form = document.getElementById(formId);
-    var element1 = document.createElement("input");
-    element1.type = "hidden";
+  addJSONVarToForm: function addJSONVarToForm(formId, jsonVar) {
+    const form = document.getElementById(formId);
+    const element1 = document.createElement('input');
+    element1.type = 'hidden';
     element1.value = JSON.stringify(jsonVar);
-    element1.name = "json_var";
+    element1.name = 'json_var';
     form.appendChild(element1);
   },
 
   // Return the index of an LI tag in a UL/OL list.
-  getListIndex: function(node) {
-    var childs = node.parentNode.childNodes;
-    for (i = 0; i < childs.length; i++) {
-      if (node == childs[i]) break;
-    }
-    return i;
-  }
+  getListIndex: function getListIndex(node) {
+    const childs = node.parentNode.children; // childNodes;
+    let i = 0;
+    let index;
+    Array.from(childs).forEach((child) => {
+      i += 1;
+      if (node === child) {
+        index = i;
+      }
+    });
+    return index;
+  },
 
 };
 
